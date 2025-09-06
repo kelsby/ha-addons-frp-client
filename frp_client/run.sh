3fe114a4e6e30f2a320adde6e0832a30f478e364
kelsby <54368889+kelsby@users.noreply.github.com>
2025-09-06 23:49:28 +0300

    Update run.sh

--- frp_client/run.sh
+++ frp_client/run.sh
@@ -1,17 +1,23 @@
-#!/bin/bash
+#!/usr/bin/with-contenv bashio
 set -e
+
+SERVER_ADDR=$(bashio::config 'server_addr')
+SERVER_PORT=$(bashio::config 'server_port')
+TOKEN=$(bashio::config 'token')
+PROXY_NAME=$(bashio::config 'proxy_name')
+CUSTOM_DOMAINS=$(bashio::config 'custom_domains')
 
 cat <<EOF >/etc/frpc.ini
 [common]
-server_addr = ${SERVER_ADDR:-127.0.0.1}
-server_port = ${SERVER_PORT:-7000}
-token = ${TOKEN:-changeme}
+server_addr = ${SERVER_ADDR}
+server_port = ${SERVER_PORT}
+token = ${TOKEN}
 
-[${PROXY_NAME:-ha}]
+[${PROXY_NAME}]
 type = http
 local_ip = 127.0.0.1
 local_port = 8123
-custom_domains = ${CUSTOM_DOMAINS:-ha.example.com}
+custom_domains = ${CUSTOM_DOMAINS}
 EOF
 
 echo "Starting frpc with config:"
